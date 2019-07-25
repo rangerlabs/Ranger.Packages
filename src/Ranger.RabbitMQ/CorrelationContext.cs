c1b469c8f8859dfb33ec10e88bd37972ba9bbb15
Nick Cromwell <cromwellnick@gmail.com>
2019-07-24 22:46:18 -0400

    Encapsulate CorrelationContext

--- src/Ranger.RabbitMQ/CorrelationContext.cs
+++ src/Ranger.RabbitMQ/CorrelationContext.cs
@@ -2,17 +2,34 @@ using System;
 
 namespace Ranger.RabbitMQ {
     public class CorrelationContext {
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
-        public Guid ResourceId { get; set; }
-        public string TraceId { get; set; }
-        public string SpanContext { get; set; }
-        public string ConnectionId { get; set; }
-        public string Name { get; set; }
-        public string Origin { get; set; }
-        public string Resource { get; set; }
-        public string Culture { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public int Retries { get; set; }
+        public Guid Id { get; }
+        public Guid UserId { get; }
+        public Guid ResourceId { get; }
+        public string TraceId { get; }
+        public string SpanContext { get; }
+        public string ConnectionId { get; }
+        public string Name { get; }
+        public string Origin { get; }
+        public string Resource { get; }
+        public string Culture { get; }
+        public DateTime CreatedAt { get; }
+        public int Retries { get; }
+
+        public CorrelationContext (Guid id, Guid userId, Guid resourceId, string traceId, string spanContext, string connectionId, string name, string origin, string resource, string culture, DateTime createdAt, int retries) {
+            this.Id = id;
+            this.UserId = userId;
+            this.ResourceId = resourceId;
+            this.TraceId = traceId;
+            this.SpanContext = spanContext;
+            this.ConnectionId = connectionId;
+            this.Name = name;
+            this.Origin = origin;
+            this.Resource = resource;
+            this.Culture = culture;
+            this.CreatedAt = createdAt;
+            this.Retries = retries;
+        }
+
+        public void IncrementRetries => this.Retries++;
     }
 }
