07069c39257723fe7600a3229c6fcf232bd6c7f1
Nick Cromwell <cromwellnick@gmail.com>
2019-09-24 11:03:07 -0400

    Add enabled property

--- src/Ranger.Common/SharedKernel/ContextTenant.cs
+++ src/Ranger.Common/SharedKernel/ContextTenant.cs
@@ -5,13 +5,14 @@ namespace Ranger.Common
     public class ContextTenant
     {
         [JsonConstructor]
-        public ContextTenant(string databaseUsername, string databasePassword)
+        public ContextTenant(string databaseUsername, string databasePassword, bool enabled)
         {
             this.DatabaseUsername = databaseUsername;
             this.DatabasePassword = databasePassword;
-
+            this.Enabled = enabled;
         }
         public string DatabaseUsername { get; }
         public string DatabasePassword { get; }
+        public bool Enabled { get; set; }
     }
 }
