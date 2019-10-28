23234012ed18abf7646691233d27c583968470dd
Nick Cromwell <cromwellnick@gmail.com>
2019-10-28 01:05:16 -0400

    Reset Api Keys and Soft Delete Projects; package updates

--- src/Ranger.InternalHttpClient/ProjectsClient/ProjectsClient.cs
+++ src/Ranger.InternalHttpClient/ProjectsClient/ProjectsClient.cs
@@ -101,7 +101,7 @@ namespace Ranger.InternalHttpClient
             }
         }
 
-        public async Task<T> PutProjectAsync<T>(HttpMethod method, string domain, string projectId, string jsonContent)
+        public async Task<T> PutProjectAsync<T>(string domain, string projectId, string jsonContent)
         {
             if (String.IsNullOrWhiteSpace(domain))
             {
@@ -116,7 +116,7 @@ namespace Ranger.InternalHttpClient
             {
                 return new HttpRequestMessage()
                 {
-                    Method = method,
+                    Method = HttpMethod.Put,
                     RequestUri = new Uri(httpClient.BaseAddress, $"/{domain}/project/{projectId}"),
                     Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
                 };
@@ -129,7 +129,68 @@ namespace Ranger.InternalHttpClient
             throw new HttpClientException<T>(apiResponse);
         }
 
-        public async Task<T> PostProjectAsync<T>(HttpMethod method, string domain, string jsonContent)
+        public async Task<T> ApiKeyResetAsync<T>(string domain, string projectId, string environment, string jsonContent)
+        {
+            if (String.IsNullOrWhiteSpace(domain))
+            {
+                throw new ArgumentException($"{nameof(domain)} cannot be null or whitespace.");
+            }
+            if (String.IsNullOrWhiteSpace(projectId))
+            {
+                throw new ArgumentException($"{nameof(projectId)} cannot be null or whitespace.");
+            }
+            if (String.IsNullOrWhiteSpace(environment))
+            {
+                throw new ArgumentException($"{nameof(environment)} cannot be null or whitespace.");
+            }
+            if (String.IsNullOrWhiteSpace(jsonContent))
+            {
+                throw new ArgumentException($"{nameof(jsonContent)} cannot be null or whitespace.");
+            }
+            Func<HttpRequestMessage> httpRequestMessageFactory = (() =>
+            {
+                return new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Put,
+                    RequestUri = new Uri(httpClient.BaseAddress, $"/{domain}/project/{projectId}/{environment}/reset"),
+                    Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+                };
+            });
+            var apiResponse = await SendAsync<T>(httpRequestMessageFactory);
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                return apiResponse.ResponseObject;
+            }
+            throw new HttpClientException<T>(apiResponse);
+        }
+
+        public async Task SoftDeleteProjectAsync(string domain, string projectId)
+        {
+            if (String.IsNullOrWhiteSpace(domain))
+            {
+                throw new ArgumentException($"{nameof(domain)} cannot be null or whitespace.");
+            }
+            if (String.IsNullOrWhiteSpace(projectId))
+            {
+                throw new ArgumentException($"{nameof(projectId)} cannot be null or whitespace.");
+            }
+            Func<HttpRequestMessage> httpRequestMessageFactory = (() =>
+            {
+                return new HttpRequestMessage()
+                {
+                    Method = HttpMethod.Delete,
+                    RequestUri = new Uri(httpClient.BaseAddress, $"/{domain}/project/{projectId}"),
+                };
+            });
+            var apiResponse = await SendAsync(httpRequestMessageFactory);
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                return;
+            }
+            throw new HttpClientException(apiResponse);
+        }
+
+        public async Task<T> PostProjectAsync<T>(string domain, string jsonContent)
         {
             if (String.IsNullOrWhiteSpace(domain))
             {
@@ -144,7 +205,7 @@ namespace Ranger.InternalHttpClient
             {
                 return new HttpRequestMessage()
                 {
-                    Method = method,
+                    Method = HttpMethod.Post,
                     RequestUri = new Uri(httpClient.BaseAddress, $"/{domain}/project"),
                     Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
                 };
