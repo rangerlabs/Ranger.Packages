15e9e6bf25bcebd38d3f08952b9c2a80bd1a2b87
Nick Cromwell <cromwellnick@gmail.com>
2020-02-22 14:00:39 -0500

    Transfer state enum

---
+++ src/Ranger.Common/SharedKernel/PrimaryOwnerTransferStateEnum.cs
@@ -0,0 +1,11 @@
+namespace Ranger.Common
+{
+    public enum PrimaryOwnerTransferStateEnum
+    {
+        Pending = 0,
+        Refused = 1,
+        Accepted = 2,
+        Cancelled = 3,
+        Rejected = 4
+    }
+}
