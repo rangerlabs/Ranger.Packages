5e4c3089f2ad0ff9c9fa5586d5b4b329e65528b9
Nick Cromwell <nick.cromwell@rangerlabs.io>
2020-10-29 23:55:03 -0400

    update http client

--- src/Ranger.InternalHttpClient/GeofencesHttpClient/IGeofencesHttpClient.cs
+++ src/Ranger.InternalHttpClient/GeofencesHttpClient/IGeofencesHttpClient.cs
@@ -7,7 +7,7 @@ namespace Ranger.InternalHttpClient
     public interface IGeofencesHttpClient
     {
         Task<RangerApiResponse<long>> GetAllActiveGeofencesCount(string tenantId, CancellationToken cancellationToken = default);
-        Task<RangerApiResponse<T>> GetAllGeofencesByProjectId<T>(string tenantId, Guid projectId, CancellationToken cancellationToken = default)
+        Task<RangerApiResponse<T>> GetGeofencesByProjectId<T>(string tenantId, Guid projectId, string orderBy, string sortOrder, int page, int pageCount, CancellationToken cancellationToken = default(CancellationToken))
             where T : class;
     }
 }
